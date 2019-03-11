bc2a2e23515b0bd9d17d2b8464187ce4b7afe0ca
wrkfmbb <kingapecherz@gmail.com>
2019-03-11 14:38:30 +0100

    Add files

---
+++ Lab01/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Person> people = new ObservableCollection<Person>
+        {
+            new Person { Name = "P1", Age = 1 },
+            new Person { Name = "P2", Age = 2 }
+        };
+
+        public ObservableCollection<Person> Items
+        {
+            get => people;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+        
+        private void AddNewPersonButton_Click(object sender, RoutedEventArgs e)
+        {
+            people.Add(new Person { Age = int.Parse(ageTextBox.Text), Name = nameTextBox.Text });
+        }
+
+        private void BtnImg_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Title = "Select your picture...";
+            openFile.Filter = "All files|*.jpg;*.jpeg;*.png|" + "JPEG (*.jpg;*.jpeg)|*jpg;*jpeg|" + "Portable Network Graphic (*.png)|*.png";
+            if(openFile.ShowDialog()==true)
+            {
+                ImgPerson.Source = new BitmapImage(new Uri(openFile.FileName));     
+            }
+        }
+    }
+}
