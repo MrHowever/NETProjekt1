5a412f927b97b04fd7f1967964fcd5a9d89e606a
wrkfmbb <kingapecherz@gmail.com>
2019-03-26 03:49:34 +0100

    Got JSON.NET

---
+++ Lab01/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Config
+    {
+        public static async Task<News.RootObject> Deserialize(string Uri)
+        {
+            var json = await ConnectionAPI.GetAsync(Uri);
+            return 
+
+        }
+    }
+}
