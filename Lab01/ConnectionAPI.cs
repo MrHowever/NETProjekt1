4b9451c1a3698f26352ee4e4c3f94be4373d1cdb
wrkfmbb <kingapecherz@gmail.com>
2019-03-26 03:29:22 +0100

    Getting connection with api

---
+++ Lab01/ConnectionAPI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class ConnectionAPI
+    {
+        public static async Task<string> GetAsync(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+
+            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return await reader.ReadToEndAsync();
+            }
+
+        }
+    }
+}
