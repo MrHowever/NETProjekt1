510736fcb305c60fab1fe560be3285df57020c00
MrHowever <matilabedzki@wp.pl>
2019-05-04 14:10:25 +0200

    XD

---
+++ Lab01/Date.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    public class Date
+    {
+        public int Year, Month, Day;
+        public Date(String textDate)
+        {
+            var components = textDate.Split('-');
+            int.TryParse(components[0], out Year);
+            int.TryParse(components[1], out Month);
+            int.TryParse(components[2], out Day);
+        }
+
+        //public static int CompareDates(Date date1, Date date2)
+    }
+
+    public class DateComparator : IComparer<Date>
+    {
+        public int Compare(Date date01, Date date02)
+        {
+            Date date1 = date01 as Date;
+            Date date2 = date02 as Date;
+
+            if (date1.Year < date2.Year)
+                return -1;
+            else if (date1.Year > date2.Year)
+                return 1;
+            else
+            {
+                if (date1.Month < date2.Month)
+                    return -1;
+                else if (date1.Month < date2.Month)
+                    return 1;
+                else
+                {
+                    if (date1.Day < date2.Day)
+                        return -1;
+                    else if (date1.Day > date2.Day)
+                        return 1;
+                    else
+                        return 0;
+                }
+            }
+        }
+    }
+    }
---
+++ Lab01/Review.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Review
+    {
+        public Date Date;
+        public int Rating;
+
+        public Review(String rating, String date)
+        {
+            int.TryParse(rating, out Rating);
+            Date = new Date(date);
+        }
+    }
+}
